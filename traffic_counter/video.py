600b19975cbd4619d63fcd9cfeadb63d4bbb18ef
Bryan Culbertson <bryan.culbertson@gmail.com>
2021-08-31 18:54:42 -0700

    Add basic live video stream to motion jpeg restreaming (#5)
    * Add basic video to motion jpeg streaming
    
    * Fix formatting

--- traffic_counter/video.py
+++ traffic_counter/video.py
@@ -1,15 +1,52 @@
 import pathlib
+from typing import Iterator
 
 import cv2
 
 
 def snapshot_to_file(video_url: str, filepath: pathlib.Path) -> None:
     cam = cv2.VideoCapture(video_url)
-    f, image = cam.read()
+    success, frame = cam.read()
 
-    if not f:
-        raise Exception("Could not read image from stream")
+    if not success:
+        raise Exception("Could not read frame from stream")
 
     cam.release()
 
-    cv2.imwrite(str(filepath), image)
+    cv2.imwrite(str(filepath), frame)
+
+
+def snapshot_to_image(video_url: str, format: str = ".jpg") -> tuple:
+    cam = cv2.VideoCapture(video_url)
+    success, frame = cam.read()
+
+    if not success:
+        raise Exception("Could not read frame from stream")
+
+    cam.release()
+
+    success, image = cv2.imencode(format, frame)
+
+    if not success:
+        raise Exception("Could not encode frane as image")
+
+    return image
+
+
+def video_as_images(video_url: str, format: str = ".jpg") -> Iterator[tuple]:
+    cam = cv2.VideoCapture(video_url)
+
+    while True:
+        success, frame = cam.read()
+
+        if not success:
+            break
+
+        success, image = cv2.imencode(format, frame)
+
+        if not success:
+            break
+
+        yield image
+
+    cam.release()
