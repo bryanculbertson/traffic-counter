52ed820a26ff8d05709a05b4ff3141fca59a206c
Bryan Culbertson <bryan.culbertson@gmail.com>
2021-09-01 15:19:04 -0700

    Add threaded camera (#6)
    * wip
    
    * Threaded camera
    
    * Fix lint

---
+++ traffic_counter/camera.py
@@ -0,0 +1,96 @@
+import abc
+import threading
+import time
+from collections.abc import Generator
+from typing import Optional
+
+import cv2
+import numpy as np
+
+
+class BaseCamera(abc.ABC):
+    def __init__(
+        self,
+        *,
+        output_fps: float = 25.0,
+        camera_fps: float = 25.0,
+    ) -> None:
+        """Start the background camera thread"""
+        self._output_fps = output_fps
+        self._camera_fps = camera_fps
+        self._event = threading.Event()
+        self._frame: Optional[np.ndarray] = None
+        self._thread: Optional[threading.Thread] = None
+
+    def start(self) -> None:
+        self._thread = threading.Thread(target=self._run)
+        self._thread.daemon = True
+        self._thread.start()
+
+    def frames(self) -> Generator[np.ndarray, None, None]:
+        """Return the current camera frame."""
+        while True:
+            if self._frame is not None:
+                yield self._frame
+
+            time.sleep(1.0 / self._output_fps)
+
+    @abc.abstractmethod
+    def _calculate_frame(self) -> Optional[np.ndarray]:
+        """Generator that returns frames from the camera."""
+        raise NotImplementedError()
+
+    def _run(self) -> None:
+        """Camera background thread."""
+        while True:
+            frame = self._calculate_frame()
+            if frame is None:
+                raise Exception("Error calculating frame")
+
+            self._frame = frame
+            time.sleep(1.0 / self._camera_fps)
+
+
+class OpenCVCamera(BaseCamera):
+    def __init__(
+        self,
+        source: str,
+        *,
+        format: str = ".jpg",
+        output_fps: float = 25.0,
+    ) -> None:
+        self._format = format
+        self._camera = cv2.VideoCapture(source)
+
+        if not self._camera.isOpened():
+            raise Exception("Could not start camera.")
+
+        camera_fps = 30.0
+
+        prop_fps = self._camera.get(cv2.CAP_PROP_FPS)
+        if prop_fps and prop_fps > 0.0:
+            camera_fps = prop_fps / 2.0
+
+        super().__init__(output_fps=output_fps, camera_fps=camera_fps)
+
+    def __del__(self) -> None:
+        if self._camera is not None:
+            self._camera.release()
+            self._camera = None
+
+    def _calculate_frame(self) -> Optional[np.ndarray]:
+        if self._camera is None:
+            print("Camera is not initialized")
+            return None
+
+        success, unencoded_frame = self._camera.read()
+        if not success:
+            print("Cannot read from camera")
+            return None
+
+        success, encoded_frame = cv2.imencode(self._format, unencoded_frame)
+        if not success:
+            print("Cannot encode image from camera")
+            return None
+
+        return encoded_frame
--- traffic_counter/server.py
+++ traffic_counter/server.py
@@ -9,7 +9,7 @@ import fastapi
 import fastapi.templating
 import pydantic
 
-from traffic_counter import video
+from traffic_counter import camera
 
 app = fastapi.FastAPI()
 templates = fastapi.templating.Jinja2Templates(directory="traffic_counter/templates")
@@ -26,24 +26,32 @@ def get_settings() -> Settings:
     return Settings()
 
 
+source_camera = None
+
+
 @app.get("/")
 async def read_root(request: fastapi.Request) -> fastapi.Response:
     return templates.TemplateResponse("index.html", context={"request": request})
 
 
-def streamer(video_url: str) -> Iterator[bytes]:
-    for image in video.video_as_images(video_url):
+def streamer(source: camera.BaseCamera) -> Iterator[bytes]:
+    for frame in source.frames():
         yield (
             b"--frame\r\n"
-            b"Content-Type: image/jpeg\r\n\r\n" + bytearray(image) + b"\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" + frame.tobytes() + b"\r\n"
         )
 
 
 @app.get("/video")
 async def video_endpoint(range: str = fastapi.Header(None)) -> fastapi.Response:
-    settings = get_settings()
+    global source_camera
+
+    if not source_camera:
+        settings = get_settings()
+        source_camera = camera.OpenCVCamera(settings.video_url)
+        source_camera.start()
 
     return fastapi.responses.StreamingResponse(
-        streamer(settings.video_url),
+        streamer(source_camera),
         media_type="multipart/x-mixed-replace;boundary=frame",
     )
